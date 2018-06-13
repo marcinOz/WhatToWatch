446a872c93c14c5c8fcf055d371ea10684a27974
Marcin Oziemski <oziemski.marcin@gmail.com>
2018-06-13 14:56:33 +0200

    Activity scopes and dependencies for AAC

--- app/src/main/java/pl/oziem/whattowatch/splash/SplashModule.kt
+++ app/src/main/java/pl/oziem/whattowatch/splash/SplashModule.kt
@@ -3,6 +3,7 @@ package pl.oziem.whattowatch.splash
 import dagger.Module
 import dagger.Provides
 import pl.oziem.datasource.dataprovider.DataProvider
+import pl.oziem.whattowatch.di.ActivityScope
 import pl.oziem.whattowatch.sharedpref.SharedPreferenceMediator
 
 /** Created by marcinoziem on 12/03/2018 WhatToWatch.
@@ -10,9 +11,11 @@ import pl.oziem.whattowatch.sharedpref.SharedPreferenceMediator
 @Module
 class SplashModule {
   @Provides
+  @ActivityScope
   fun provideMainView(activity: SplashActivity): SplashContract.View = activity
 
   @Provides
+  @ActivityScope
   fun provideMainPresenter(movieListView: SplashContract.View,
                            dataProvider: DataProvider,
                            sharedPrefMediator: SharedPreferenceMediator)
